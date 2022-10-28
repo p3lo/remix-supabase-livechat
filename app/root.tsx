f470f17547b1f06e30ca9d9935925b4637061272
p3lo <mysiacik@mysiacik.com>
2022-10-29 00:06:21 +0200

    wip navbar

--- app/root.tsx
+++ app/root.tsx
@@ -1,34 +1,22 @@
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
-import { json } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "@remix-run/react";
-import { useTranslation } from "react-i18next";
-import { useChangeLanguage } from "remix-i18next";
+import React from 'react';
 
-import { i18nextServer } from "~/integrations/i18n";
+import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
+import { useChangeLanguage } from 'remix-i18next';
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getBrowserEnv } from "./utils/env";
+import { i18nextServer } from '~/integrations/i18n';
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: tailwindStylesheetUrl },
-];
+import tailwindStylesheetUrl from './styles/tailwind.css';
+import { getBrowserEnv } from './utils/env';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Remix Notes',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -42,16 +30,58 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function App() {
   const { env, locale } = useLoaderData<typeof loader>();
   const { i18n } = useTranslation();
-
+  // const [theme, setTheme] = React.useState<string>(() => {
+  //   // getting stored value
+  //   const saved = localStorage.getItem('theme');
+  //   const initialValue = JSON.parse(saved as string);
+  //   return initialValue || 'dark';
+  // });
   useChangeLanguage(locale);
 
   return (
-    <html lang={locale} dir={i18n.dir()} className="h-full">
+    <html lang={locale} dir={i18n.dir()} className="h-full" data-theme="dark">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="relative h-full">
+        <div className="sticky top-0 z-50 flex space-x-2 border-b border-gray-500 navbar bg-base-100/80">
+          <div className="justify-start flex-none">
+            <Link to="/" className="text-xl font-bold normal-case">
+              LiveChat
+            </Link>
+          </div>
+          <div className="flex justify-center space-x-2 grow">
+            <Link to="/" className="normal-case">
+              MenuItem1
+            </Link>
+            <div className="divider divider-horizontal" />
+            <Link to="/" className="normal-case">
+              MenuItem2
+            </Link>
+            <div className="divider divider-horizontal" />
+            <Link to="/" className="normal-case">
+              MenuItem3
+            </Link>
+          </div>
+          <div className="flex justify-end flex-none space-x-2">
+            <Link to="/join" className="normal-case btn w-[100px] btn-sm">
+              Register
+            </Link>
+            <Link to="/login" className="normal-case btn w-[100px] btn-outline btn-sm">
+              Login
+            </Link>
+            <label className="swap swap-rotate">
+              <input type="checkbox" className="invisible" />
+              <svg className="w-6 h-6 fill-current swap-on" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+              </svg>
+              <svg className="w-6 h-6 fill-current swap-off" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+              </svg>
+            </label>
+          </div>
+        </div>
         <Outlet />
         <ScrollRestoration />
         <script
