fb5e289a79c3e710f731928857513b523b02f824
p3lo <mysiacik@mysiacik.com>
2023-01-11 23:23:12 +0100

    added plus button to chat for invite

--- app/modules/livekit/components/ChatMessage.tsx
+++ app/modules/livekit/components/ChatMessage.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import { AiOutlinePlusCircle } from 'react-icons/ai';
 import { BsCameraVideo } from 'react-icons/bs';
 
 import type { MessageChat } from './Chat';
 
 export function ChatMessage({ message, room_name }: { message: MessageChat; room_name: string }) {
   return (
-    <div className="flex space-x-1 items-center">
-      {room_name === message.user.nickname ? <BsCameraVideo className="text-sm" /> : null}
+    <div className="flex items-center space-x-1">
+      {room_name === message.user.nickname ? <BsCameraVideo className="w-5 text-sm" /> : null}
+
+      {room_name === message.user.nickname ? (
+        <AiOutlinePlusCircle className="w-5 text-sm hover:cursor-pointer" />
+      ) : null}
+
       <p style={{ color: message.user.chat_color }} className={`text-sm font-bold ml-2`}>
         {message.user.nickname}
       </p>
-      <p key={message.id} className="pl-1 flex flex-row text-sm ">
+      <p key={message.id} className="w-[90%] overflow-hidden text-sm text-ellipsis">
         {message.message}
       </p>
     </div>
